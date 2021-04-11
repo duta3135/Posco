d2efadd46278f0519bb7dd50e6ffabc61e5229be
duta3135 <75598408+duta3135@users.noreply.github.com>
2021-04-11 09:46:11 +0700

    Add files via upload

---
+++ posco/script.js
@@ -0,0 +1,8 @@
+function openMenu(){
+    document.getElementById('nav').style.width =  "43%";
+    document.getElementById('openbtn').style.opacity =  "0";
+}
+function closeMenu(){
+    document.getElementById('nav').style.width =  "0";
+    document.getElementById('openbtn').style.opacity =  "100%";
+}
